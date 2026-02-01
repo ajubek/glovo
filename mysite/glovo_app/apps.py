6e237afe8986c02d7a20f0d4764565f9d0b5cd7b
ajubek <zyldyzbekalihanov915@gmail.com>
2026-02-01 08:33:45 +0600

    'finish'

--- mysite/glovo_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GlovoAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'glovo_app'
--- mysite/glovo_app/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.pagination import PageNumberPagination
-
-
-class CategoryPagination(PageNumberPagination):
-    page_size = 3
-
-class ProductPagination(PageNumberPagination):
-    page_size = 2
